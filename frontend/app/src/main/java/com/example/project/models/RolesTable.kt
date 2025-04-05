096c734fb22f6e8dccc6e8726c61b0cfeba1bcb4
RohithCherukuri816 <rohithcherukuri816@gmail.com>
2025-04-05 21:53:40 +0530

    Delete frontend directory

--- frontend/app/src/main/java/com/example/project/models/RolesTable.kt
+++
@@ -1,9 +0,0 @@
-package com.example.project.models
-
-import com.google.gson.annotations.SerializedName
-
-data class RolesTable(
-    @SerializedName("roleid") val roleId: Int,
-    @SerializedName("rolename") val roleName: String,
-    @SerializedName("description") val description: String
-)
