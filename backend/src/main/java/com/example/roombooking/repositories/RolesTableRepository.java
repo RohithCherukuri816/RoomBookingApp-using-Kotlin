515d10beb846a76e85fc727fc8e02d85acc3af63
Rohith <rohithcherukuri816@gmail.com>
2025-04-05 21:56:13 +0530

    Initial commit of RoomBookingApp project

---
+++ backend/src/main/java/com/example/roombooking/repositories/RolesTableRepository.java
@@ -0,0 +1,7 @@
+package com.example.roombooking.repositories;
+
+import com.example.roombooking.models.RolesTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesTableRepository extends JpaRepository<RolesTable, Integer> {
+}
